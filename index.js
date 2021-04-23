b64ea4f17e72b96df8ca66885d25e0c8093eb66b
William Yurkow <williamy@tlcengine.com>
2021-04-23 13:06:17 -0400

    I have the Apollo server running and mongo is connected.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const { MONGODB } = require('./config')
 
 const typeDefs = gql`
   type Query {
@@ -9,7 +12,7 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello Word!',
+    sayHi: () => 'Hello Word!!!!!!!!!',
   },
 }
 
@@ -18,6 +21,12 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`)
-})
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Mongodb Connected')
+    return server.listen({ port: 5000 })
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`)
+  })
