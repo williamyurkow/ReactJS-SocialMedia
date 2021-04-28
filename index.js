6cbc349e8964b4bc3f8e6fbe4bc9345cf328f79b
William Yurkow <williamy@tlcengine.com>
2021-04-27 20:16:36 -0400

    added some folders/files to be a little bit more organized with graphql and resolvers.

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const { ApolloServer } = require('apollo-server')
-const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
-const Post = require('./models/Postmodel')
+const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config')
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find()
-        return posts
-      } catch (err) {
-        throw new Error(err)
-      }
-    },
-  },
-}
 
 const server = new ApolloServer({
   typeDefs,
